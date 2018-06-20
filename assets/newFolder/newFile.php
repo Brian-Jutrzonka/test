60816b3e0c9e3fa5fb69790475a6ff2ac9ae042b
Brian Jutrzonka <bjutrzonka@gmail.com>
2018-06-20 13:21:36 -0500

    Added content

--- assets/newFolder/newFile.php
+++ assets/newFolder/newFile.php
@@ -1,1 +1,9 @@
 HEy, here is a new file!
+
+Unsafe Acts	12 - 13.44
+Unsafe Conditions	33 - 36.96
+Near Misses	3 - 3.36
+Something Safe	38 - 42.56
+Opportunities for Improvement	7 - 7.84
+Safety Assessments	19 - 21.28
+Total	112
